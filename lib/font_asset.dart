4811904d0316e1f97954da262443d46479276237
Moritz <mosum@google.com>
2025-12-30 15:56:28 +0100

    analyze

--- lib/font_asset.dart
+++ lib/font_asset.dart
@@ -1,2 +1,2 @@
-export 'src/font_asset_base.dart' show FontAsset, FontAssetExt;
 export 'src/extension.dart' show FontAssetsExtension;
+export 'src/font_asset_base.dart' show FontAsset, FontAssetExt;
