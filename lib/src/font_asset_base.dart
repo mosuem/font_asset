63c0b0f98800b649b7c03b6ffa12a2c7fd84bb86
Moritz <mosum@google.com>
2025-12-30 16:01:44 +0100

    make weight nullable

--- lib/src/font_asset_base.dart
+++ lib/src/font_asset_base.dart
@@ -7,7 +7,7 @@ class FontAsset {
     required this.file,
     required this.fontFamily,
     required this.package,
-    required this.weight,
+    this.weight,
   });
   factory FontAsset.fromEncoded(EncodedAsset encodedAsset) {
     final Map<String, Object?> encoding = encodedAsset.encoding;
@@ -15,14 +15,14 @@ class FontAsset {
       file: Uri.parse(encoding[_fileKey]! as String),
       fontFamily: encoding[_fontFamilyKey]! as String,
       package: encoding[_packageKey]! as String,
-      weight: encoding[_weightKey]! as int,
+      weight: encoding[_weightKey] as int?,
     );
   }
 
   final Uri file;
   final String fontFamily;
   final String package;
-  final int weight;
+  final int? weight;
 
   static const _fileKey = 'file';
   static const _weightKey = 'weight';
@@ -34,7 +34,7 @@ class FontAsset {
       _fileKey: file.toFilePath(windows: false),
       _fontFamilyKey: fontFamily,
       _packageKey: package,
-      _weightKey: weight,
+      if (weight != null) _weightKey: weight,
     });
   }
 }
